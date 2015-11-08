56e63ffb7a5709d8a328947168ed2c473b9e5e92
blaz <blaz@menems.net>
2015-11-09 00:41:27 +0100

    feat: recursive

--- index.js
+++ index.js
@@ -5,15 +5,33 @@ const fs = require('fs');
 const path = require('path');
 const Router = require('koa-66');
 
+const walk = (modulesPath, excludeDir, callback) => {
+    fs.readdirSync(modulesPath).forEach(file => {
+        const newPath = path.join(modulesPath, file);
+
+        const stat = fs.statSync(newPath);
+
+        if (stat && stat.isFile() && /^index\.(js|coffee)$/.test(file))
+            return callback(newPath);
+
+        if (stat && stat.isDirectory() && file !== excludeDir)
+            return walk(newPath, excludeDir, callback);
+    });
+};
+
 module.exports = _path => {
-    debug('root path: %s', _path);
+
+    if (!_path) throw new Error('_path is required');
+
+    const stat = fs.statSync(_path);
+    if(!stat.isDirectory()) throw new Error('_path must be a directory');
+
     const router = new Router();
 
-    fs.readdirSync(_path).forEach(m => {
-        const file = path.join(_path, m);
-        const _p = '/' + m;
-        debug('mount %s on %s', file, _p );
-        router.mount('/' + m, require(file));
+    walk(_path, null, m => {
+        const route_path = path.dirname(m).split(_path).pop();
+        debug('mount on %s', route_path);
+        router.mount(route_path, require(m));
     });
 
     return router;
